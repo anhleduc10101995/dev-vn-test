9967b62f5c86499af14e9313071e9117b7551b78
anh leduc <71745533+anhleduc10101995@users.noreply.github.com>
2022-03-12 13:32:04 -0500

    environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,35 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PhotosModule } from './photos/photos.module';
 import { Photo } from './photos/photo.entity';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-      type: 'mysql',
-      socketPath: '/cloudsql/hallowed-cortex-343019:northamerica-northeast1:trinity',
-      username: 'trinity',
-      password: '1234',
-      database: 'trinity',
-      entities: [Photo],
-      synchronize: true,
+  imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
+        cache: true,
+        envFilePath: process.env.NODE_ENV === 'production'
+          ? ["env/production.env"]
+          : ["env/local.env", "env/development.env"]
+      }),
+      TypeOrmModule.forRoot(process.env.NODE_ENV === 'production' ? {
+        type: 'mysql',
+        socketPath: '/cloudsql/hallowed-cortex-343019:northamerica-northeast1:trinity',
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        entities: [Photo],
+        synchronize: true,
+      } : {
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        entities: [Photo],
+        synchronize: true,
     }),
     PhotosModule
   ],
